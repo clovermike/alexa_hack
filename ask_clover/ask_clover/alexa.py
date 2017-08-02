3623ebcec35bfb2e440688b9d4a7fe5aeccbc5a9
Lavinia Karl <lavinia.karl@cloverhealth.com>
2017-08-02 16:21:29 -0700

    Make example skeleton of some functions

--- ask_clover/ask_clover/alexa.py
+++ ask_clover/ask_clover/alexa.py
@@ -1,3 +1,48 @@
-ALEXA_APP_ID_DEFAULT="Your Amazon Alexa App ID DEFAULT"
-ALEXA_APP_ID_OTHER="Your Amazon Alexa App ID OTHER" # for each app
-ALEXA_REQUEST_VERIFICATON=True # Enables/Disable request verification
+"""Alexa app"""
+
+import os
+from django_alexa.api import fields, intent, ResponseBuilder
+
+
+ALEXA_APP_ID_DEFAULT = os.environ["ALEXA_APP_ID_DEFAULT"]
+ALEXA_REQUEST_VERIFICATON = True # Enables/Disable request verification
+DEBUG=True
+
+@intent
+def call_clover(session):
+    """
+    Call clover health
+
+    ---
+
+    call clover
+    """
+    return ResponseBuilder.create_response(message="Calling Clover Health",
+                                           end_session=False,
+                                           launched=True)
+
+@intent
+def benefits(session, benefit=None):
+    """
+    Ask clover about benefits
+
+    ---
+
+    clover what are my benefits
+    clover tell me about my benefits
+    clover tell me about my plan
+    clover tell me about my {benefit} benefits
+    clover tell me about my {benefit} plan
+    """
+
+    kwargs = {}
+    if benefit:
+        kwargs['message'] = " Here is the information about your {0} plan.".format(benefit)
+        #TODO add function that will look up benefits and recite them
+    else:
+        kwargs['reprompt'] = "What benefits do you want to hear about?"
+        # options should be all, or specific benefit keyword
+    if session.get('launched'):
+        kwargs['end_session'] = False
+        kwargs['launched'] = session['launched']
+    return ResponseBuilder.create_response(**kwargs)
